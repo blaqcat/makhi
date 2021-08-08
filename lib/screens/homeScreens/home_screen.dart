a1827049b86b7a1830af869d1ea189064ff930b1
blaqcat <ngakana@protonmail.com>
2021-08-08 18:36:08 +0200

    Day 2 3nd commit

--- lib/screens/homeScreens/home_screen.dart
+++ lib/screens/homeScreens/home_screen.dart
@@ -13,6 +13,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  final screens = [
+    Center(child: Text('Home', style: TextStyle(fontSize: 60)),),
+    Center(child: Text('Search', style: TextStyle(fontSize: 60)),),
+    Center(child: Text('Business Tools', style: TextStyle(fontSize: 60)),),
+    Center(child: Text('Profile', style: TextStyle(fontSize: 60)),),
+
+
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,24 +37,38 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
         centerTitle: true,
       ),
-      body: Center(child: Text('Home', style: TextStyle(fontSize: 60)),),
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        showUnselectedLabels: true,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
+            backgroundColor: Colors.amberAccent,
           ),
           BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Home',
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Colors.blue,
           ),
           BottomNavigationBarItem(
-          icon: Icon(Icons.business_center_rounded),
-          label: 'Home',
+            icon: Icon(Icons.business_center),
+            label: 'Tools'
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: "Settings"
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Colors.green,
           )
 
         ],
